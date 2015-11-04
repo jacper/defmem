a5d75dda337c66784ba8e87880253b1a909e3b0c
jacbel <jacob.bellmund@gmail.com>
2015-11-04 16:34:03 +0100

    jacbel's edit of distance judgments and maptest scripts
    edited scripts from jacper

--- defmem_distance.m
+++ defmem_distance.m
@@ -1,4 +1,5 @@
 clear all
+
 %% SET UP
 % which subjects
 subjects =1:16;
@@ -7,42 +8,60 @@ subjects =1:16;
 nSubs       = length(subjects);
 nPositions  = 8;
 nTrials     = 28;
-coordinatesPos = [-374 -68; -187 0; -374 68; -561 0; 374 -68; 561 0; 374 68; 187 0]; %origin= middle
+coordinatesPos = [-374 -68; -187 0; -374 68; -561 0; 374 -68; 561 0; 374 68; 187 0]; % origin= middle
+colors{1}       = [184 43 34] /255;     % donders red
+colors{2}       = [255 186 0] /255;     % darkYellow
+colors{3}       = [0 177 177] /255;     % petrolGreen
+colors{4}       = [96 136 135]/255;     % mpi
+colors{5}       = [123 83 104]/255;     % plumb
 
-%initialize
+% initialize variables
 p1          = nan(nTrials,nSubs);
 p2          = nan(nTrials,nSubs);
 trialOnset  = nan(nTrials,nSubs);
 respTime    = nan(nTrials,nSubs);
 givenDist   = nan(nTrials,nSubs);
+jitter      = nan(nTrials,nSubs);
+fixOnset    = nan(nTrials,nSubs);
+cursorYpos  = nan(nTrials,nSubs);
 p1x         = nan(nTrials,nSubs);
 p1y         = nan(nTrials,nSubs);
 p2x         = nan(nTrials,nSubs);
 p2y         = nan(nTrials,nSubs);
 corrValues  = nan(nSubs,1);
 pCorr       = nan(nSubs,1);
+meanDistBroad = nan(nSubs,1);
+meanDistNarrow = nan(nSubs,1);
+
 
 %% GET DATA
 for iSub = 1:nSubs;
     subID = subjects(iSub);
     
     % where is the data
-    datPth = '~/defmem/data/distanceJudgment';
+    datPth = '/home/memspa/jacper/defmem/data/distanceJudgment';
     fName = sprintf('%02d_distance_judgments.txt', subID);
 
-    % read logfiles
+    % read logfile
     logDat = dlmread(fullfile(datPth,fName),'\t');
     
-    % store variables
-    p1(:,iSub)          = logDat(:,1);
-    p2(:,iSub)          = logDat(:,2);
-    trialOnset(:,iSub)  = logDat(:,5)/10000;
-    givenDist(:,iSub)   = (logDat(:,6)-min(logDat(:,6)))/range(logDat(:,6)); % normalized from 0-1
-    respTime(:,iSub)    = logDat(:,8)/1000;
+    % get variables                                 % LOGFILE STRUCTURE
+    p1(:,iSub)          = logDat(:,1);              % column 1 = object/location ID 1
+    p2(:,iSub)          = logDat(:,2);              % column 2 = object/location ID 2
+    jitter(:,iSub)      = logDat(:,3);              % column 3 = jitter
+    fixOnset(:,iSub)    = logDat(:,4);              % column 4 = between trials fixation                    
+    trialOnset(:,iSub)  = logDat(:,5)/10000;        % column 5 = trial start
+    givenDist(:,iSub)   = logDat(:,6);              % column 6 = given distance (x cursor position at response)
+    cursorYpos(:,iSub)  = logDat(:,7);              % column 7 = y cursor position at response
+    respTime(:,iSub)    = logDat(:,8)/1000;         % column 8 = RT
+    
+    % normalize responses
+    givenDist(:,iSub)   = givenDist(:,iSub) + 400;%(givenDist(:,iSub)-min(givenDist(:,iSub)))/range(givenDist(:,iSub)); % normalized from 0-1
 end
 
 % get average RT
 meanRT = mean(respTime);
+
 
 %% ANALYZE DISTANCES
 
@@ -56,34 +75,33 @@ for iTrial = 1:nTrials
 end
 
 % get correct directions in radians
-[~, corrDist] = cart2pol(p2x-p1x, p2y-p1y);
-corrDist    = corrDist/max(corrDist(:)); %% noramlized from 0-1 (closest and furthest possible distances)
+[~, correctDist] = cart2pol(p2x-p1x, p2y-p1y);
+correctDist    = correctDist/max(correctDist(:)); %% noramlized from 0-1 (closest and furthest possible distances)
 
-%average error for each participant= how good they are 
-average_error_subs=mean(givenDist-corrDist);
+% average error for each participant= how good they are 
+avgErrorSub = mean(givenDist-correctDist); % i am not sure if this is a valid thing to do 
 
+% initialize figure for scatter plots
+fig01 = figure;
 
 for iSub = 1:nSubs
-    dondersRed  = [184 43 34]/255;
-
-    %correlate given distance with actual distance
-    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),corrDist(:,iSub));
+    
+    % correlate given distance with actual distance
+    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),correctDist(:,iSub));
     
     %% PLOT SCATTER WITH LEAST SQUARES LINE
     subplot(4,4,iSub)
-    scatter(givenDist(:,iSub), corrDist(:,iSub), 30, dondersRed,'filled')
-    xlabel('remembered distance 0-1', 'Fontsize', 8)
-    ylabel('actual distance 0-1', 'Fontsize', 8)
+    scatter(correctDist(:,iSub), givenDist(:,iSub), 30, colors{1},'filled')
+    xlabel('actual distance', 'Fontsize', 8)
+    ylabel('remembered distance', 'Fontsize', 8)
     axis square
-    title(sprintf('\nr=%.2f, p=%.2f, mean RT=%.1fs', corrValues(iSub), pCorr(iSub), meanRT(iSub)), 'Fontsize', 8)
+    title(sprintf('\nr=%.2f, p=%.2f', corrValues(iSub), pCorr(iSub)), 'Fontsize', 8)
     h = lsline;
-    set((h),'color', dondersRed, 'LineWidth',2)
+    set((h),'color', colors{1}, 'LineWidth',2)
 end
 
 
-
-
-%%INTERESTING DISTANCES TO LOOK AT
+%% INTERESTING DISTANCES TO LOOK AT
 paralel_y.all=(p1==3 & p2==1 ...
             |  p1==1 & p2==3 ...
             |  p1==7 & p2==5 ...
@@ -112,22 +130,59 @@ paralel_x0.longer.narrow=(p1==2 & p2==6 ...
 paralel_x0.longest=(p1==4 & p2==6 ...
                   | p1==6 & p2==4);
 
+% all trials within narrow/broad              
+broadJudgments  = p1<=4 & p2<=4;
+narrowJudgments = p1>=5 & p2>=5;
+
+% mean and SEM
+for iSub = 1:nSubs
+    
+    meanDistBroad(iSub)     = mean(givenDist(broadJudgments(:,iSub),iSub));
+    meanDistNarrow(iSub)    = mean(givenDist(narrowJudgments(:,iSub),iSub));
+end
+
+% test for significance
+[h, p, ~, stats] = ttest(meanDistBroad, meanDistNarrow);
+
+% calculate SEM
+semDistBroad    = std(meanDistBroad)/sqrt(nSubs);
+semDistNarrow    = std(meanDistNarrow)/sqrt(nSubs);
+
+
+%% barplot of remembered distance broad vs. narrow
+fig02 = figure;
+hold on
+bar(1, mean(meanDistBroad(:)), 0.8, 'FaceColor', colors{1})  
+bar(2, mean(meanDistNarrow(:)), 0.8, 'FaceColor', colors{2})
+set(gca, 'XTick', [1 2], 'XTickLabel', {'broad', 'narrow'});
+ylabel('Remembered Distance');
+
+% add errorbars
+h1=errorbar([mean(meanDistBroad(:)) mean(meanDistNarrow(:))], [semDistBroad semDistNarrow]);
+hc = get(h1, 'Children');
+set(hc(1), 'Marker', 'none', 'LineStyle', 'none')   % remove lines for data from errorbar
+set(hc(2), 'color', [0 0 0], 'LineWidth', 3)        % set error bars to black and make them thick
+errorbar_tick(h1, 0);                               % remove error bar end markers
+
+%% JACBEL CHECKED UNTIL HERE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%%%%%playing with them
 %%plotting
 
-figure
+fig03 = figure;
 hold on
-scatter(corrDist(paralel_y.narrow(:,:)),givenDist(paralel_y.narrow(:,:)), 50, 'r', 'o');
-scatter(corrDist(paralel_y.broad(:,:)),givenDist(paralel_y.broad(:,:)), 50, 'b', 'x');
+scatter(correctDist(paralel_y.narrow(:,:)),givenDist(paralel_y.narrow(:,:)), 50, 'r', 'o');
+scatter(correctDist(paralel_y.broad(:,:)),givenDist(paralel_y.broad(:,:)), 50, 'b', 'x');
 
-scatter(corrDist(paralel_x0.narrow(:,:)), givenDist(paralel_x0.narrow(:,:)), 50, 'r', 'o');
-scatter(corrDist(paralel_x0.broad(:,:)), givenDist(paralel_x0.broad(:,:)), 50, 'b', 'x');
-scatter(corrDist(paralel_x0.middle(:,:)), givenDist(paralel_x0.middle(:,:)), 50, 'g', '*');
+scatter(correctDist(paralel_x0.narrow(:,:)), givenDist(paralel_x0.narrow(:,:)), 50, 'r', 'o');
+scatter(correctDist(paralel_x0.broad(:,:)), givenDist(paralel_x0.broad(:,:)), 50, 'b', 'x');
+scatter(correctDist(paralel_x0.middle(:,:)), givenDist(paralel_x0.middle(:,:)), 50, 'g', '*');
 
-scatter(corrDist(paralel_x0.longer.narrow(:,:)), givenDist(paralel_x0.longer.narrow(:,:)), 50, 'r', 'o');
-scatter(corrDist(paralel_x0.longer.broad(:,:)), givenDist(paralel_x0.longer.broad(:,:)), 50, 'b', 'x');
+scatter(correctDist(paralel_x0.longer.narrow(:,:)), givenDist(paralel_x0.longer.narrow(:,:)), 50, 'r', 'o');
+scatter(correctDist(paralel_x0.longer.broad(:,:)), givenDist(paralel_x0.longer.broad(:,:)), 50, 'b', 'x');
 
-scatter(corrDist(paralel_x0.longest(:,:)), givenDist(paralel_x0.longest(:,:)), 50, 'g', '*');
+scatter(correctDist(paralel_x0.longest(:,:)), givenDist(paralel_x0.longest(:,:)), 50, 'g', '*');
 
 
 %%means
@@ -142,7 +197,6 @@ mean_error_x.longer_narrow=mean(givenDist(paralel_x0.longer.narrow));
 mean_error_x.longest=mean(givenDist(paralel_x0.longest));
 
 
-
 %%%%%%%%PLOTTING GIVEN, CORRECT DISTANCES VS p1
 figure
 hold on
@@ -150,7 +204,7 @@ for iSub = 1:nSubs
     dondersRed  = [184 43 34]/255;
 
     %correlate given distance with actual distance
-    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),corrDist(:,iSub));
+    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),correctDist(:,iSub));
     
     %% PLOT SCATTER WITH LEAST SQUARES LINE
     subplot(4,4,iSub)
@@ -170,11 +224,11 @@ for iSub = 1:nSubs
     dondersRed  = [184 43 34]/255;
 
     %correlate given distance with actual distance
-    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),corrDist(:,iSub));
+    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),correctDist(:,iSub));
     
     %% PLOT SCATTER WITH LEAST SQUARES LINE
     subplot(4,4,iSub)
-    scatter(p1(:,iSub), corrDist(:,iSub))
+    scatter(p1(:,iSub), correctDist(:,iSub))
     xlabel('position p1', 'Fontsize', 8)
     ylabel('correct distance 0-1', 'Fontsize', 8)
     %axis square
@@ -191,7 +245,7 @@ for iSub = 1:nSubs
     dondersRed  = [184 43 34]/255;
 
     %correlate given distance with actual distance
-    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),corrDist(:,iSub));
+    [corrValues(iSub), pCorr(iSub)] = corr(givenDist(:,iSub),correctDist(:,iSub));
     
     %% PLOT SCATTER WITH LEAST SQUARES LINE
     subplot(4,4,iSub)
@@ -205,7 +259,7 @@ for iSub = 1:nSubs
 %     set(gca, 'YLim',[0 1], 'XLim', [0.2 1.6],'XTick', [.4 1 1.6], 'YTick', [0 .5 1])
     hold on
     subplot(4,4,iSub)
-    scatter(p1(:,iSub), corrDist(:,iSub))
+    scatter(p1(:,iSub), correctDist(:,iSub))
     xlabel('position p1', 'Fontsize', 8)
     ylabel('correct distance 0-1', 'Fontsize', 8)
     %axis square
--- defmem_maptest_new.m
+++ defmem_maptest_new.m
@@ -9,11 +9,20 @@ nSubs       = length(subjects);
 nPositions  = 8;
 Positions = [-374 -68; -187 0; -374 68; -561 0; 374 -68; 561 0; 374 68; 187 0];
 markerTypes = {'o' '*' '+' 'h' 's' 'd'  'p' 'x'};
-colors      = [184 43 34; 196 118 75; 108 52 31;...
-                108 31 61; 196 76 143; 0 128 128; 0 0 128;...
-                0 128 0; 255 140 0; 128 128 0]/255;
-            
-iPosition = 1:nPositions;            
+colors      = [184 43 34;...
+               255 186 0;...
+               0 177 177;...
+               96 136 135;...
+%                123 83 104;...
+               108 52 31;...
+               108 31 61;...
+               196 76 143;...
+               0 128 128;...
+               0 0 128;...
+               0 128 0;...
+               255 140 0;...
+               128 128 0]...
+               /255;
 
 % initialize variables
 trialStart  = nan(nPositions,nSubs);
@@ -33,7 +42,7 @@ for iSub = 1:nSubs;
     subID = subjects(iSub);
     
     % where is the data
-    datPth = '~/defmem/data/maptest/';
+    datPth = '/home/memspa/jacper/defmem/data/maptest/';
     fName = sprintf('%02d_map_log.txt', subID);
 
     % read logfiles
@@ -54,11 +63,8 @@ for iSub = 1:nSubs;
 
 end
 
-% transform positions to same space as our map
-xSelect;
 
-%%getting trapezoid, same dimensions as presentation
-            
+%% getting trapezoid, same dimensions as presentation           
 corners = [4751, 505; ...
     4751, -505; ...
     -4751, -2286; ...
@@ -67,50 +73,59 @@ corners = [4751, 505; ...
 corners(:,1)=corners(:,1)/6.34;
 corners(:,2)=corners(:,2)/7.38;
 
+%% plot trapezoid and given locations across particpants (color = location)
 fig01 = figure;
 hold on
+axis equal
+axis off
 line(corners(1:2,1), corners(1:2,2), 'LineWidth', 5, 'Color', [0 0 0])
 line(corners([2, 3],1), corners([2, 3],2) , 'LineWidth', 5, 'Color', [0 0 0])
 line(corners(3:4,1), corners(3:4,2), 'LineWidth', 5, 'Color', [0 0 0])
 line(corners([1, 4],1), corners([1, 4],2), 'LineWidth', 5, 'Color', [0 0 0])
-axis equal
-scatter(Positions(:,1), Positions(:,2), 250, 'r', markerTypes{1});
-%circles(0,0,250, 'facecolor','none', 'LineWidth', 3);
-%circles(0,0,500, 'facecolor','none', 'LineWidth', 3);
-%circles(0,0,750, 'facecolor','none', 'LineWidth', 3);
-%circles(0,0,1000, 'facecolor','none', 'LineWidth', 3);
-%circles(0,0,1250, 'facecolor','none', 'LineWidth', 3);
-%scatter(corners(:,1), corners(:,2))
-
-%hold on
-
-% loop over buildingsconfidenceconfidenceconfidence
-%for iPosition = 1:nPositions
-%    dondersRed  = [184 43 34]/255;
-%
-%    % plot correct building
-%    h = scatter(Positions(iPosition,1), Positions(iPosition,2),350, markerTypes{iPosition});
-%    set(h, 'MarkerEdgeColor', [0 0 0], 'LineWidth', 4)
-%    
-%    % plot given locations
-%    g = scatter(xSelect(iPosition,:), ySelect(iPosition,:),250, colors(1:nSubs,:), markerTypes{iPosition});
-%    set(g, 'LineWidth', 3)
-%   
-%end
-
-hold on 
 
 for iPosition=1:8;
-    iSub=1:16;
-    dondersRed  = [184 43 34]/255;
     
-    %subplot (4,4,iSub)
-    scatter(xSelect(iPosition,:), ySelect(iPosition,:), 50, dondersRed, 'filled')
+    % plot correct location as open circle
+    scatter(Positions(iPosition,1), Positions(iPosition,2), 300, colors(iPosition,:), markerTypes{1}, 'LineWidth', 4);
+    
+    % plot response locations of all participants as filled circles
+    scatter(xSelect(iPosition,:), ySelect(iPosition,:), 50,  colors(iPosition,:), 'filled')
+
+end
+
+
+%% SUBPLOT selected positions for each subject
+fig02 = figure;
+hold on
+
+for iSub = 1:nSubs;
+    
+    % open subplot
+    c=subplot(4,4,iSub);
+    hold on
+    
+    % plot trapezoid
+    line(corners(1:2,1), corners(1:2,2), 'LineWidth', 3, 'Color', [0 0 0])
+    line(corners([2, 3],1), corners([2, 3],2) , 'LineWidth', 3, 'Color', [0 0 0])
+    line(corners(3:4,1), corners(3:4,2), 'LineWidth', 3, 'Color', [0 0 0])
+    line(corners([1, 4],1), corners([1, 4],2), 'LineWidth', 3, 'Color', [0 0 0])
+    
+    for iPosition= 1:nPositions;
+        
+        % plot the response location for this position 
+        scatter(xSelect(iPosition,iSub), ySelect(iPosition,iSub), 50, colors(iPosition,:), 'filled');   
+        
+        % plot correct location as open circle
+        scatter(Positions(iPosition,1), Positions(iPosition,2), 50, colors(iPosition,:), markerTypes{1}, 'LineWidth', 4);
+
+    end
     axis equal
+    axis off
 end
 
 
 %% CALCULATE SOME DESCRIPTIVE STATS
+
 % get the average RTs
 meanRT = mean(respTime);
 
@@ -124,37 +139,27 @@ for iSub = 1:nSubs
     [~,errSize(:,iSub)] = cart2pol(xDiff(:,iSub),yDiff(:,iSub));
 end
 
-% normalize the error (FNWI north entrance - Trigon)
-[~,refDist] = cart2pol(835-242,242-127);
-errSize = errSize / refDist;
-
 % average error per participant
-meanError = mean(errSize);
+meanErrorSub = mean(errSize);
+meanErrorPos = mean(errSize,2);
 
-%%%%%SUBPLOTTING 16 trapezoids %%%%%%%%
-dondersRed  = [184 43 34]/255;
 
-figure
+%% PLOT TRAPEZOID WITH AVERAGE ERROR PER LOCATION
+fig03 = figure;
 hold on
-for iSub = 1:nSubs;
+axis equal
+axis off
+
+% plot trapezoid
+line(corners(1:2,1), corners(1:2,2), 'LineWidth', 3, 'Color', [0 0 0])
+line(corners([2, 3],1), corners([2, 3],2) , 'LineWidth', 3, 'Color', [0 0 0])
+line(corners(3:4,1), corners(3:4,2), 'LineWidth', 3, 'Color', [0 0 0])
+line(corners([1, 4],1), corners([1, 4],2), 'LineWidth', 3, 'Color', [0 0 0])
+
+for iPosition = 1:nPositions
     
-    %% SUBPLOT selected positions for each subject
-    c=subplot(4,4,iSub);
-    hold on
+    % plot circle at correct location with size corresponding to error
+    circles(Positions(iPosition,1), Positions(iPosition,2), meanErrorPos(iPosition)/2,...
+        'edgecolor', colors(iPosition,:), 'facecolor','none', 'LineWidth', 5);
     
-    line(corners(1:2,1), corners(1:2,2), 'LineWidth', 3, 'Color', [0 0 0])
-    line(corners([2, 3],1), corners([2, 3],2) , 'LineWidth', 3, 'Color', [0 0 0])
-    line(corners(3:4,1), corners(3:4,2), 'LineWidth', 3, 'Color', [0 0 0])
-    line(corners([1, 4],1), corners([1, 4],2), 'LineWidth', 3, 'Color', [0 0 0])
-    
-    
-    for iPosition= 1:nPositions;
-        hold on 
-        scatter(xSelect(iPosition,iSub), ySelect(iPosition,iSub), 50, dondersRed, markerTypes{iPosition});   %%%%not working different markerTypes neither colors
-        scatter(Positions(iPosition,1), Positions(iPosition,2), 50, 'r', markerTypes{iPosition});
-        %set(h, 'MarkerEdgeColor', [0 0 0], 'LineWidth', 4)
-    end
-    axis equal
 end
-
-
